8c28c47dd0a6da64d719132c3d7981c6214d7684
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-06-16 13:07:33 +0200

    fix: correct dependency on Microsoft.Extensions.DependencyInjection.Abstractions

--- src/Incremental.Common.Sourcing.Messaging/IMessage.cs
+++ src/Incremental.Common.Sourcing.Messaging/IMessage.cs
@@ -1,5 +1,8 @@
 namespace Incremental.Common.Sourcing.Messaging;
 
+/// <summary>
+/// Attach to a record or class to make it shareable through <see cref="IMessageBus"/>.
+/// </summary>
 public interface IMessage
 {
     
